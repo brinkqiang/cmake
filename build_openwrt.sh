1f953e61131f3c49fc76068671d6f8cd7b0afa7e
brinkqiang <brink.qiang@gmail.com>
2024-07-02 02:34:20 +0800

    add

--- build_openwrt.sh
+++ build_openwrt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Utilities\cmlibarchive\CMakeLists.txt mod OPTION(ENABLE_OPENSSL "Enable use of OpenSSL" ON) -> OPTION(ENABLE_OPENSSL "Enable use of OpenSSL" OFF)
+# Utilities\cmcurl\CMakeLists.txt add set(CMAKE_USE_OPENSSL OFF) 
+# bootstrap mod cmake_options="-DCMAKE_BOOTSTRAP=1" -> cmake_options="-DCMAKE_BOOTSTRAP=1 -DCMAKE_USE_OPENSSL=OFF"
 find . -name "*.cmake.in" -exec sed -i  "s/-lrt//g" '{}' \;
 find . -name "*.cmake.in" -exec sed -i  "s/-ldl//g" '{}' \;
 sed -i  "s/-ldl//g" bootstrap &&  sed -i  "s/-lrt//g" bootstrap
