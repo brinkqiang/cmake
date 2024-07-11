17c9839bc32e5d0aa15d5d289e9814d24e2815fa
brinkqiang <brink.qiang@gmail.com>
2024-07-11 14:15:33 +0800

    add

--- build_openwrt.sh
+++ build_openwrt.sh
@@ -7,10 +7,8 @@ find . -name "*.cmake.in" -exec sed -i  "s/-lrt//g" '{}' \;
 find . -name "*.cmake.in" -exec sed -i  "s/-ldl//g" '{}' \;
 sed -i  "s/-ldl//g" bootstrap &&  sed -i  "s/-lrt//g" bootstrap
 ./bootstrap --prefix=/usr
-make
 find . -name "link.txt" -exec sed -i  "s/-ldl//g" '{}' \;
 find . -name "link.txt" -exec sed -i  "s/-lrt//g" '{}' \;
 find . -name Makefile2 -exec sed -i  "s/-ldl//g" '{}' \;
 find . -name Makefile2 -exec sed -i  "s/-lrt//g" '{}' \;
-make
 make install
