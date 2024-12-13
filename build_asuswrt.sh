924134c6b107d279f6f275a76b04a3b6865a073e
brinkqiang <brink.qiang@gmail.com>
2024-12-13 13:45:52 +0800

    add

--- build_asuswrt.sh
+++ build_asuswrt.sh
@@ -4,6 +4,6 @@
 # Utilities\cmcurl\CMakeLists.txt add set(CMAKE_USE_OPENSSL OFF) 
 # bootstrap mod cmake_options="-DCMAKE_BOOTSTRAP=1" -> cmake_options="-DCMAKE_BOOTSTRAP=1 -DCMAKE_USE_OPENSSL=OFF"
 
-./bootstrap --prefix=/usr
+./bootstrap --prefix=/mnt/router-data/entware/bin
 
 make install
