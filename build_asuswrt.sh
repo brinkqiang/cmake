0f8c6a84bba6f698eafdfe005af6bb14639ab467
brinkqiang <brink.qiang@gmail.com>
2024-12-13 12:07:56 +0800

    add

---
+++ build_asuswrt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Utilities\cmlibarchive\CMakeLists.txt mod OPTION(ENABLE_OPENSSL "Enable use of OpenSSL" ON) -> OPTION(ENABLE_OPENSSL "Enable use of OpenSSL" OFF)
+# Utilities\cmcurl\CMakeLists.txt add set(CMAKE_USE_OPENSSL OFF) 
+# bootstrap mod cmake_options="-DCMAKE_BOOTSTRAP=1" -> cmake_options="-DCMAKE_BOOTSTRAP=1 -DCMAKE_USE_OPENSSL=OFF"
+
+./bootstrap --prefix=/usr
+
+make install
